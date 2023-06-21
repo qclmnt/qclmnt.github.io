3b4c6180c19f1eca2f6dfab2d9e7b4799eb9a372
Quentin CLEMENT <quentinclement@MacBook-Air-de-Quentin-2.local>
2023-06-21 22:35:48 +0200

    step 2 : random list

--- game.js
+++ game.js
@@ -3,16 +3,44 @@
  * Copyright 2019 Google LLC. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
 function initialize() {
-  const fenway = { lat: 42.345573, lng: -71.098326 };
+
+  const points = [  {lat: 44.6364301, lng: 1.3770812},
+                    {lat: 43.316416, lng: -0.3797836},
+                    {lat: 48.8689155, lng: 2.3524931},
+                    {lat: 48.6451142, lng: -2.0184227},
+                    {lat: 47.9446014, lng: 0.2335304},
+                    {lat: 49.2398422, lng: 4.0224285},
+                    {lat: 49.2489852, lng: 3.9056319},
+                    {lat: 45.8818883, lng: 6.1745673},
+                    {lat: 46.7308276, lng: 6.3565855},
+                    {lat: 49.7623064, lng: 5.0633475},
+                    {lat: 49.0049055, lng: 2.5769586},
+                    {lat: 44.6566433, lng: -1.2390916},
+                    {lat: 49.2541972, lng: 3.9314264},
+                    {lat: 49.2947421, lng: 4.1192805},
+                    {lat: 45.9230314, lng: 6.8704662},
+                    {lat: 45.7586568, lng: 4.8358745},
+                    {lat: 44.6939981, lng: 3.0351179},
+                    {lat: 50.6394372, lng: 3.0583644},
+                    {lat: 47.1980479, lng: 4.0635723},];
+
+
+  const randomPoint = points[getRandomInt(points.length)];
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: fenway,
+    center: randomPoint,
     zoom: 14,
   });
   const panorama = new google.maps.StreetViewPanorama(
     document.getElementById("pano"),
     {
-      position: fenway,
+      position: randomPoint,
       pov: {
         heading: 34,
         pitch: 10,
